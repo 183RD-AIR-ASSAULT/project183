a902ab36c505df81e043c226bb776fa278450746
YonV <yonvclaw@gmail.com>
2026-01-07 10:06:36 -0700

    signs

---
+++ addons/183rd_Dev_Mod/Scripts/Game/Weapon/Attachments/AttachmentOpticsRIS1913_183rd.c
@@ -0,0 +1,25 @@
+// -----------------------------------------------------------------------------
+// AttachmentOpticsRIS1913_183rd.c
+// Caliber-specific RIS1913 optic attachment types for 183rd weapons.
+// -----------------------------------------------------------------------------
+
+class AttachmentOpticsRIS1913_762Class : AttachmentOpticsRIS1913Class {}
+class AttachmentOpticsRIS1913_762 : AttachmentOpticsRIS1913 {}
+class AttachmentOpticsRIS1913_762ShortClass : AttachmentOpticsRIS1913ShortClass {}
+class AttachmentOpticsRIS1913_762Short : AttachmentOpticsRIS1913Short {}
+class AttachmentOpticsRIS1913_762MediumClass : AttachmentOpticsRIS1913MediumClass {}
+class AttachmentOpticsRIS1913_762Medium : AttachmentOpticsRIS1913Medium {}
+
+class AttachmentOpticsRIS1913_68Class : AttachmentOpticsRIS1913Class {}
+class AttachmentOpticsRIS1913_68 : AttachmentOpticsRIS1913 {}
+class AttachmentOpticsRIS1913_68ShortClass : AttachmentOpticsRIS1913ShortClass {}
+class AttachmentOpticsRIS1913_68Short : AttachmentOpticsRIS1913Short {}
+class AttachmentOpticsRIS1913_68MediumClass : AttachmentOpticsRIS1913MediumClass {}
+class AttachmentOpticsRIS1913_68Medium : AttachmentOpticsRIS1913Medium {}
+
+class AttachmentOpticsRIS1913_338Class : AttachmentOpticsRIS1913Class {}
+class AttachmentOpticsRIS1913_338 : AttachmentOpticsRIS1913 {}
+class AttachmentOpticsRIS1913_338ShortClass : AttachmentOpticsRIS1913ShortClass {}
+class AttachmentOpticsRIS1913_338Short : AttachmentOpticsRIS1913Short {}
+class AttachmentOpticsRIS1913_338MediumClass : AttachmentOpticsRIS1913MediumClass {}
+class AttachmentOpticsRIS1913_338Medium : AttachmentOpticsRIS1913Medium {}
